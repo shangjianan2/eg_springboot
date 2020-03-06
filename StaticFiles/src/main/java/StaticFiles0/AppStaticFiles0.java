707f539214913e3247e5f828bc71af326483ac61
1978707987@qq.com <1978707987@qq.com>
2020-03-06 16:42:46 +0800

    added StaticFiles.StaticFiles0

---
+++ StaticFiles/src/main/java/StaticFiles0/AppStaticFiles0.java
@@ -0,0 +1,11 @@
+package StaticFiles0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStaticFiles0 {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStaticFiles0.class, args);
+    }
+}
